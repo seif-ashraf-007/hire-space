1b6748c4e53a795fe2371fd949ce4bd0ce0f0dc5
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-10-01 09:50:13 +0300

    feat: add Swagger documentation setup in main application file

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const swagger = new DocumentBuilder().setVersion('1.0').build();
+  const documentation = SwaggerModule.createDocument(app, swagger);
+  // http://localhost:8000/api
+  SwaggerModule.setup('api', app, documentation);
 
   await app.listen(process.env.PORT ?? 8000);
 }
