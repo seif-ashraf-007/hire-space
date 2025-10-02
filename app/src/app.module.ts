9e51cca04d8bad520db01ba694f120ed65081bc4
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-10-02 07:08:21 +0300

    feat: enhance application configuration and database integration
    - Updated package.json to include cross-env for environment variable management in scripts.
    - Added @nestjs/config and @nestjs/typeorm dependencies for configuration and database support.
    - Configured global ConfigModule and TypeOrmModule in app.module.ts for PostgreSQL database connection.
    - Enhanced Swagger documentation in main.ts with title, description, and server information.
    - Changed default application port from 8000 to 7000.
    - Added new utility files enums.ts and types.ts for future use.

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('DB_DATABASE'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        port: configService.get('DB_PORT'),
+        host: configService.get('DB_HOST'),
+        synchronize: process.env.NODE_ENV !== 'production' ? true : false,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
